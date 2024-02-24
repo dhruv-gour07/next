67d0f9d647a57e245056af5ffabc6e5b3c1ee2e4
dhruv-gour07 <161121431+dhruv-gour07@users.noreply.github.com>
2024-02-24 16:18:37 +0530

    Update floating-navbar.tsx

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -118,13 +118,13 @@ export const FloatingNav = ({
         )}
       >
         {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"> */}
-          <Link href="/"
+          {/* <Link href="/"
           className={cn(
             "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
         )}
           >Home</Link>
-          <span className="absolute inset-x-10 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-slate-50 to-transparent h-px" />
         {/* </button> */}
+          <span className="absolute inset-x-10 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-slate-50 to-transparent h-px" /> 
         {navItems.map((navItem: any, idx: number) => (
           <Link
             key={`link=${idx}`}
