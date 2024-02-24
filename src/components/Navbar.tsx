c26fc2a6b05b9461d147825dcbe1de4ea020ad8e
dhruv-gour07 <161121431+dhruv-gour07@users.noreply.github.com>
2024-02-24 16:14:56 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,11 @@ import { FloatingNav } from "../components/ui/floating-navbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 export default function FloatingNavDemo() {
   const navItems = [
+      {
+            name: "Home",
+            link: "/",
+            icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+          },
     {
       name: "About",
       link: "/about",
