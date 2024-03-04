1e21dc6f793441d00e18dfee56d1b9b93e981ba7
dhruv-gour07 <161121431+dhruv-gour07@users.noreply.github.com>
2024-03-04 18:00:38 +0530

    Update page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,1 +1,28 @@
+import Navbar from "@/components/Navbar";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
 
+const myWork = () => {
+  const myLink = "https://naayra-travels.vercel.app/";
+  return (
+    <>
+      <Navbar />
+      <div className="mx-auto flex items-center justify-center mt-28 flex-col">
+        <Link href={myLink}>
+          <p className="bg-gradient-to-b mb-7 from-slate-50 to-neutral-800 font-extrabold text-transparent bg-clip-text text-2xl">
+            Click here to go to: Naayra Travels Website
+          </p>
+          <Image
+            src="/naayra-travels.png"
+            width={500}
+            height={500}
+            alt="ScreenShot image of naayra travels website"
+          />
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default myWork;
